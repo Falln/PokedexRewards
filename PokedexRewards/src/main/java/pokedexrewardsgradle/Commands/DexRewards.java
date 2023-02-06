56aa5344cd75291443af9b6ba1d233a2e6f8e755
Falln <isaaccole8806@gmail.com>
2023-02-06 00:03:26 -0600

    Initial Commit

---
+++ PokedexRewards/src/main/java/pokedexrewardsgradle/Commands/DexRewards.java
@@ -0,0 +1,41 @@
+package pokedexrewardsgradle.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginLogger;
+
+// "/dexReward claim"
+//This command allows someone to type dexReward claim to claim a pokedex reward if they have one available
+public class DexRewards implements CommandExecutor {
+
+    PluginLogger logger;
+
+    public DexRewards(PluginLogger logger) {
+        super();
+        this.logger = logger;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        //Check if the command sent is for pokedexrewards
+        if (cmd.getName().equalsIgnoreCase("dexReward")){
+            //Check Arguments sent with the command
+            //"claim" command. Allows the user to check if they have claimed a dexreward and give items if they have
+            if (args[0] == "claim") {
+                //Check if it's a player sending the message
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage("This command can only be run by a player.");
+                } else {
+                    //Player player = (Player) sender;
+                    logger.info("Well i mean it works up to where I think it would - Falln");
+                    //Check if they have a rewards and give it to them
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    
+}
---
+++ PokedexRewards/src/main/java/pokedexrewardsgradle/Events/OnCatchEvent.java
@@ -0,0 +1,25 @@
+package pokedexrewardsgradle.Events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginLogger;
+
+import com.pixelmonmod.pixelmon.api.events.CaptureEvent;
+
+//TODO This class is for detecting when a pokemon is caught and then letting the
+//catcher know if they have a pokdex reward to claim
+public final class OnCatchEvent implements Listener {
+
+    PluginLogger logger;
+
+    public OnCatchEvent(PluginLogger logger) {
+        super();
+        this.logger = logger;
+    }
+    
+    @EventHandler
+    public void onPokemonCaught(CaptureEvent.SuccessfulCapture successfulCapture) {
+        logger.info("A pokemon was successfully caught!");
+
+    }
+}
